c6678dff09cd552daba1f946e477d83967de90f0
chicoumbetov <shyngysmaaan@gmail.com>
2023-03-01 20:08:37 +0100

    :recycle: ObjectDetection convert to tsx

--- src/ObjectDetection.tsx
+++ src/ObjectDetection.tsx
@@ -5,6 +5,111 @@ import "@tensorflow/tfjs-backend-cpu";
 //import "@tensorflow/tfjs-backend-webgl";
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
 
+const ObjectDetection = (props: any) => {
+  const fileInputRef: any = useRef();
+  const imageRef: any = useRef();
+  const [imgData, setImgData] = useState(null);
+  const [predictions, setPredictions] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+
+  const isEmptyPredictions = !predictions || predictions.length === 0;
+
+  const openFilePicker = () => {
+    if (fileInputRef.current) fileInputRef.current.click();
+  };
+
+  const normalizePredictions = (predictions, imgSize) => {
+    if (!predictions || !imgSize || !imageRef) return predictions || [];
+    return predictions.map((prediction) => {
+      const { bbox } = prediction;
+      const oldX = bbox[0];
+      const oldY = bbox[1];
+      const oldWidth = bbox[2];
+      const oldHeight = bbox[3];
+
+      const imgWidth = imageRef.current.width;
+      const imgHeight = imageRef.current.height;
+
+      const x = (oldX * imgWidth) / imgSize.width;
+      const y = (oldY * imgHeight) / imgSize.height;
+      const width = (oldWidth * imgWidth) / imgSize.width;
+      const height = (oldHeight * imgHeight) / imgSize.height;
+
+      return { ...prediction, bbox: [x, y, width, height] };
+    });
+  };
+
+  const detectObjectsOnImage = async (imageElement, imgSize) => {
+    const model = await cocoSsd.load({});
+    const predictions = await model.detect(imageElement, 6);
+    const normalizedPredictions = normalizePredictions(predictions, imgSize);
+    console.log("normalizedPredictions 1 : ", normalizedPredictions);
+    setPredictions(normalizedPredictions);
+    console.log("Predictions: ", predictions);
+  };
+
+  const readImage = (file) => {
+    return new Promise((rs, rj) => {
+      const fileReader = new FileReader();
+      fileReader.onload = () => rs(fileReader.result);
+      fileReader.onerror = () => rj(fileReader.error);
+      fileReader.readAsDataURL(file);
+    });
+  };
+
+  const onSelectImage = async (e) => {
+    setPredictions([]);
+    setLoading(true);
+
+    const file = e.target.files[0];
+    const imgData: any = await readImage(file);
+    setImgData(imgData);
+
+    const imageElement = document.createElement("img");
+    imageElement.src = imgData;
+
+    imageElement.onload = async () => {
+      const imgSize = {
+        width: imageElement.width,
+        height: imageElement.height,
+      };
+      await detectObjectsOnImage(imageElement, imgSize);
+      setLoading(false);
+    };
+  };
+
+  return (
+    <ObjectDetectorContainer>
+      {imgData && (
+        <DetectorContainer>
+          {imgData && <TargetImg src={imgData} ref={imageRef} />}
+          {!isEmptyPredictions &&
+            predictions.map((prediction: any, idx: number) => (
+              <TargetBox
+                key={idx}
+                x={prediction.bbox[0]}
+                y={prediction.bbox[1]}
+                width={prediction.bbox[2]}
+                height={prediction.bbox[3]}
+                classType={prediction.class}
+                score={prediction.score * 100}
+              />
+            ))}
+        </DetectorContainer>
+      )}
+
+      <HiddenFileInput
+        type="file"
+        ref={fileInputRef}
+        onChange={onSelectImage}
+      />
+      <SelectButton onClick={openFilePicker} className="uploadImage">
+        {isLoading ? "Recognizing..." : "Select Image"}
+      </SelectButton>
+    </ObjectDetectorContainer>
+  );
+};
+
 const ObjectDetectorContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -69,105 +174,4 @@ const TargetBox = styled.div`
   }
 `;
 
-export function ObjectDetector(props) {
-  const fileInputRef = useRef();
-  const imageRef = useRef();
-  const [imgData, setImgData] = useState(null);
-  const [predictions, setPredictions] = useState([]);
-  const [isLoading, setLoading] = useState(false);
-
-  const isEmptyPredictions = !predictions || predictions.length === 0;
-
-  const openFilePicker = () => {
-    if (fileInputRef.current) fileInputRef.current.click();
-  };
-
- const normalizePredictions = (predictions, imgSize) => {
-    if (!predictions || !imgSize || !imageRef) return predictions || [];
-    return predictions.map((prediction) => {
-      const { bbox } = prediction;
-      const oldX = bbox[0];
-      const oldY = bbox[1];
-      const oldWidth = bbox[2];
-      const oldHeight = bbox[3];
-
-      const imgWidth = imageRef.current.width;
-      const imgHeight = imageRef.current.height;
-
-      const x = (oldX * imgWidth) / imgSize.width;
-      const y = (oldY * imgHeight) / imgSize.height;
-      const width = (oldWidth * imgWidth) / imgSize.width;
-      const height = (oldHeight * imgHeight) / imgSize.height;
-
-      return { ...prediction, bbox: [x, y, width, height] };
-    });
-  };
-
-  const detectObjectsOnImage = async (imageElement, imgSize) => {
-    const model = await cocoSsd.load({});
-    const predictions = await model.detect(imageElement, 6);
-    const normalizedPredictions = normalizePredictions(predictions, imgSize);
-    console.log('normalizedPredictions 1 : ', normalizedPredictions);
-    setPredictions(normalizedPredictions);
-    console.log("Predictions: ", predictions);
-  };
-
-  const readImage = (file) => {
-    return new Promise((rs, rj) => {
-      const fileReader = new FileReader();
-      fileReader.onload = () => rs(fileReader.result);
-      fileReader.onerror = () => rj(fileReader.error);
-      fileReader.readAsDataURL(file);
-    });
-  };
-
-  const onSelectImage = async (e) => {
-    setPredictions([]);
-    setLoading(true);
-
-    const file = e.target.files[0];
-    const imgData = await readImage(file);
-    setImgData(imgData);
-
-    const imageElement = document.createElement("img");
-    imageElement.src = imgData;
-
-    imageElement.onload = async () => {
-      const imgSize = {
-        width: imageElement.width,
-        height: imageElement.height,
-      };
-      await detectObjectsOnImage(imageElement, imgSize);
-      setLoading(false);
-    };
-  };
-
-  return (
-    <ObjectDetectorContainer>
-        {imgData && <DetectorContainer>
-            {imgData && <TargetImg src={imgData} ref={imageRef} />}
-            {!isEmptyPredictions &&
-            predictions.map((prediction, idx) => (
-                <TargetBox
-                key={idx}
-                x={prediction.bbox[0]}
-                y={prediction.bbox[1]}
-                width={prediction.bbox[2]}
-                height={prediction.bbox[3]}
-                classType={prediction.class}
-                score={prediction.score * 100}
-                />
-            ))}
-        </DetectorContainer>}
-        
-        <HiddenFileInput
-            type="file"
-            ref={fileInputRef}
-            onChange={onSelectImage}
-        />
-        <SelectButton onClick={openFilePicker} className='uploadImage'>
-            {isLoading ? "Recognizing..." : "Select Image"}
-        </SelectButton>
-    </ObjectDetectorContainer>
-  );
-}
+export default ObjectDetection;
